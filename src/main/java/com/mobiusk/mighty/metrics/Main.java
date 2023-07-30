19e1cdbdcd76daf8370907fefd30f22d420f93ae
Mobius K <mail@mobiusk.com>
2023-07-30 15:38:12 -0400

    Adding minimum necessary code for program building, running, and testing

---
+++ src/main/java/com/mobiusk/mighty/metrics/Main.java
@@ -0,0 +1,9 @@
+package com.mobiusk.mighty.metrics;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
+
+}
---
+++ src/test/java/com/mobiusk/mighty/metrics/MainTest.java
@@ -0,0 +1,14 @@
+package com.mobiusk.mighty.metrics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void test() {
+    assertEquals(4, 2 + 2);
+  }
+
+}
