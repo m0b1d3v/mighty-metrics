0d4ec57d9ff5c68981a6ed127aa4779f97d9f16b
m0b1 <mail@m0b1.dev>
2023-10-07 19:55:42 -0400

    Adding interface models for commonly used strings.

---
+++ src/main/java/dev/m0b1/mighty/metrics/models/DatabaseTables.java
@@ -0,0 +1,9 @@
+package dev.m0b1.mighty.metrics.models;
+
+public interface DatabaseTables {
+  String COACH = "coach";
+  String EXERCISE = "exercise";
+  String MEMBER = "member";
+  String SCORE = "score";
+  String SCORECARD = "scorecard";
+}
---
+++ src/main/java/dev/m0b1/mighty/metrics/models/OAuth2Attributes.java
@@ -0,0 +1,6 @@
+package dev.m0b1.mighty.metrics.models;
+
+public interface OAuth2Attributes {
+  String ID = "id";
+  String NAME = "global_name";
+}
