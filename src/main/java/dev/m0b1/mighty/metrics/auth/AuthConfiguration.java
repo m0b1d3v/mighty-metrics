4b3950ba2a1f810385428a2f4860fd324f85bbd3
m0b1 <mail@m0b1.dev>
2023-12-01 09:58:28 -0500

    OAuth flow cancels now go to "/" instead of "/login".

--- src/main/java/dev/m0b1/mighty/metrics/auth/AuthConfiguration.java
+++ src/main/java/dev/m0b1/mighty/metrics/auth/AuthConfiguration.java
@@ -2,7 +2,6 @@ package dev.m0b1.mighty.metrics.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -32,7 +31,7 @@ public class AuthConfiguration {
       )
       .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
       .logout(l -> l.logoutSuccessUrl("/").permitAll())
-      .oauth2Login(Customizer.withDefaults())
+      .oauth2Login(o -> o.loginPage("/"))
       .build();
   }
 
